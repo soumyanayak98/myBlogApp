c78b95445c979096c42a462bd9d774e16c3a4bc2
soumyanayak98 <soumya.nayak@kreeti.com>
2021-08-19 21:27:40 +0530

    article listing feature added in articles#index page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,9 @@
 class ArticlesController < ApplicationController
 
+  def index
+    @articles = Article.all
+  end
+
   def show
     @article = Article.find(params[:id]) #this instance variavle available in articles#show view
   end
