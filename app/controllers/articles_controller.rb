11a05f2264ec275a6d9e04409b23ed2b02bf6799
soumyanayak98 <soumya.nayak@kreeti.com>
2021-08-20 00:31:13 +0530

    create new article added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,21 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id]) #this instance variavle available in articles#show view
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    # render plain: params[:article] # shows {"title"=>"zghf", "description"=>"zdghxd"}
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    # render plain: @article # shows #<Article:0x00007f9460106908> // @article.inspect
+    if @article.save 
+      flash[:notice] = "Article Created Successfully"
+      redirect_to article_path(@article) #redirecting to articles#show view with @article.id
+      # redirect_to @article # this is shorthand code for above
+    else
+      render new_article_path
+    end
+  end
 end
