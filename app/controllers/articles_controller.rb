eaffb24aaa8c6586d7e4619d162a1e24265cf198
soumyanayak98 <soumya.nayak@kreeti.com>
2021-08-24 18:03:55 +0530

    edit articles added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,7 +21,22 @@ class ArticlesController < ApplicationController
       redirect_to article_path(@article) #redirecting to articles#show view with @article.id
       # redirect_to @article # this is shorthand code for above
     else
-      render new_article_path
+      render new_article_path # or 'new'
     end
+  end
+
+  def edit
+    @article= Article.find(params[:id])
+  end
+
+  def update
+    @article= Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title,:description))
+      flash[:notice] = "Article Updated Successfully"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+    
   end
 end
