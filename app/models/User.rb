4a49c5f6418984db1ed04b0941dec7ea4798f3a3
soumyanayak98 <soumya.nayak@kreeti.com>
2021-08-26 21:40:52 +0530

    sign up form added

--- app/models/User.rb
+++ app/models/User.rb
@@ -1,12 +1,16 @@
 class User < ApplicationRecord
+
   before_save :downCaseEmail
-  has_many :articles
+  
+  has_many :articles, dependent: :destroy
+  has_secure_password
+
   validates :username, presence: true, length: {minimum:3 , maximum:25}, uniqueness: { case_sensitive: false }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum:150}, 
                     uniqueness: { case_sensitive: false },
                     format: {with: VALID_EMAIL_REGEX}
-  
+  validates :password, presence: true
   private
   def downCaseEmail
     self.email = email.downcase
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   root 'pages#home'
   # get '/',to: "pages#home"
-  get '/home', to: "pages#home"
+  
   get '/about', to: "pages#about"
+  
+  get '/signup', to: "users#new"
+  # post '/users', to: "users#create"
+  resources :users, except: [:new]
+
   resources :articles #, only: [:show, :index, :new, :create, :edit, :update, :delete]
 end
