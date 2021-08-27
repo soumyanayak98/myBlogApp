2327432334e7b0d1f31f17cd1b9b91b122b3544c
soumyanayak98 <soumya.nayak@kreeti.com>
2021-08-27 20:52:02 +0530

    user show page added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  before_action :set_user , only: [:show, :edit, :update]
+
+  def show
+    @articles = @user.articles
+  end
 
   def new
     @user = User.new
@@ -6,7 +11,6 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-
     if @user.save
       flash[:notice] = "Welcome #{@user.username} ,You have Successfully signed Up!"
       redirect_to articles_path
@@ -16,12 +20,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
-
     if @user.update(user_params)
       flash[:notice] = "Account successfully updated"
       redirect_to articles_path
@@ -32,6 +33,9 @@ class UsersController < ApplicationController
 
 
   private
+  def set_user
+    @user = User.find(params[:id])
+  end
   def user_params
     params.require(:user).permit(:username,:email, :password)
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def gravatar_for (user , options = {size: 100})
+    email_address = user.email.downcase
+    hash = Digest::MD5.hexdigest(email_address)
+    size = options[:size]
+    image_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
+    image_tag(image_url, alt: user.username, class: "rounded shadow mx-auto d-block")
+  end
 end
